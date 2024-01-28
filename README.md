ef65e15aa8d63c63b0d2356d869604edd0ee345c
tomasbaran <tomas.baran@rethoughtinsurance.com>
2024-01-28 16:03:58 -0600

    config: Install Flutter

---
+++ README.md
@@ -0,0 +1,3 @@
+# minimalist_timer_app_3
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/minimalist_timer_app_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.minimalist_timer_app_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
