392369d30b0f39ce25bbbe9b506a66af84714046
Tomas Baran <tomas.baran@gmail.com>
2024-01-28 17:27:18 -0600

    feat: Add first use case: PlayTimer

---
+++ lib/features/timer/domain/use_cases/play_timer.dart
@@ -0,0 +1,8 @@
+import 'package:minimalist_timer_app_3/shared/use_cases/i_use_case.dart';
+
+class PlayTimer implements IUsecase {
+  @override
+  Future call() {
+    return;
+  }
+}
---
+++ lib/shared/use_cases/i_use_case.dart
@@ -0,0 +1,3 @@
+abstract class IUsecase<T> {
+  Future<T> call();
+}
