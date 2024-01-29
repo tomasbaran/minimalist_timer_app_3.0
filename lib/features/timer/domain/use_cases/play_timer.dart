b145d4a4278d96632893600171a72af1215136b3
Tomas Baran <tomas.baran@gmail.com>
2024-01-28 20:50:33 -0600

    test-green: Add PlayTimer use case implementation

--- lib/features/timer/domain/use_cases/play_timer.dart
+++ lib/features/timer/domain/use_cases/play_timer.dart
@@ -3,8 +3,7 @@ import 'package:minimalist_timer_app_3/shared/use_cases/i_use_case.dart';
 class PlayTimer implements IUseCase<Duration, Duration> {
   @override
   Future<Duration> call(Duration duration) async {
-    // await Future.delayed(const Duration(seconds: 1));
-    // return duration - const Duration(seconds: 1);
-    return duration;
+    await Future.delayed(const Duration(seconds: 1));
+    return duration - const Duration(seconds: 1);
   }
 }
